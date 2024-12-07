567f837fea43a032a9bed5d3eddad7cd67b096dc
Naees <bubblesproximaceturion@gmail.com>
2024-12-08 03:24:14 +0800

    Recursive functions

---
+++ 008_functionsRecursive.cpp
@@ -0,0 +1,23 @@
+// factorial calculator
+#include <iostream>
+using namespace std;
+
+long factorial (long a)
+{
+    if (a>1)    // base case
+    {
+        return (a * factorial (a-1));   
+        // n! = n * (n-1) * (n-2) *  (n-3) ... * 1 
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    long number = 9;
+    cout << number << "! = " << factorial(number);
+    return 0;
+}
